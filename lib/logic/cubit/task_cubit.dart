e3883b243ba14bb6a4edb6d4866454a112916248
Anas Radi <39970013+AnasRadi@users.noreply.github.com>
2026-01-29 17:56:00 +0200

    ### 🔹 Hydrated Bloc - Hydrated Bloc State Persistence & Restoration - Hydrated Bloc automatically saves the Bloc/Cubit state to local storage. - It allows state restoration upon application restart without any additional code.

--- lib/logic/cubit/task_cubit.dart
+++ lib/logic/cubit/task_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:uuid/uuid.dart';
 
@@ -7,28 +8,39 @@ import '../../model/TaskModel.dart';
 
 part 'tasks_state.dart';
 
-class TaskCubit extends Cubit<TaskState> {
+class TaskCubit extends HydratedCubit<TaskState> {
   TaskCubit() : super(InitTask());
 
   void addTask(String title) {
-    final taskModel = TaskModel(
-      id: Uuid().v4(),
-      title: title,
-      isDone: false,
-    );
-    emit(UpdateTake([...state.taskList, taskModel]));
+    final taskModel = TaskModel(id: Uuid().v4(), title: title, isDone: false);
+    emit(UpdateTask([...state.taskList, taskModel]));
   }
 
   void removeTask(String id) {
-    emit(UpdateTake(state.taskList
-        .where((task) => task.id != id)
-        .toList()));
+    emit(UpdateTask(state.taskList.where((task) => task.id != id).toList()));
   }
 
   void checkTask(String id) {
     final List<TaskModel> newList = state.taskList.map((task) {
       return task.id == id ? task.copyWith(isDone: !task.isDone) : task;
     }).toList();
-    emit(UpdateTake(newList));
+    emit(UpdateTask(newList));
+  }
+
+  @override
+  TaskState? fromJson(Map<String, dynamic> json) {
+    final list = (json['todolist'] as List)
+        .map((e) => TaskModel.fromJson(e as Map<String, dynamic>))
+        .toList();
+    return UpdateTask(list);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(TaskState state) {
+    return {
+      'todolist': state.taskList
+          .map((todo) => todo.toJson())
+          .toList(growable: false),
+    };
   }
 }
